68eeaa5bafacb8361b6af375cd2e4cfa68a57a75
Haryordeji1 <ifeoluwa3600@gmail.com>
2025-05-04 18:32:24 +0100

    Add files via upload

---
+++ scoping and stack.py
@@ -0,0 +1,12 @@
+Outer Function Start
+Inner Function Start
+I am outside!
+I am inside!
+Outer Function End
+Inside function, x = 5
+Outside function, x = 10
+3
+2
+1
+Blast off!
+# this code would only run if there are necessary block of codes written 
